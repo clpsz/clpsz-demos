4c61f2b59892e017aee87767a2529f154adef4ef
Zuo Pengpeng <zuopengpeng@uhappybox.cn>
2015-03-04 17:46:55 +0800

    add domaini socket example

---
+++ domain_socket/main.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "socket.h"
+
+
+int main()
+{
+    pid_t pid;
+
+    pid = fork();
+    if (pid < 0)
+    {
+        perror("fork()");
+    }
+    else if (pid > 0)
+    {
+        int lsn_fd, apt_fd;
+        char buf[128];
+        int ret;
+
+        printf("parent pid %d\n", getpid());
+        lsn_fd = init_listen_server(REQUEST);
+        apt_fd = accept_client(lsn_fd);
+        ret = read(apt_fd, buf, 124);
+        if (ret > 0)
+        {
+            buf[ret] = '\0';
+            printf("%d, %s\n", getpid(), buf);
+        }
+    }
+    else
+    {
+        int fd;
+        
+        printf("child pid %d\n", getpid());
+        fd = connect_server(REQUEST);
+        if (fd > 0)
+        {
+            write(fd, "I am child", strlen("I am child.") + 1);
+        }
+    }
+
+    return 0;
+}
+
