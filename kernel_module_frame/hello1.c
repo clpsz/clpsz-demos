a33b52c5c66427bcf34b211f0cabe7b5499752d0
Pengpeng Zuo <zzzz@ubuntu.(none)>
2015-04-10 10:10:22 +0800

    add another file
    module init and exit file must in the same file as module_init() and module_exit()

--- kernel_module_frame/hello1.c
+++ kernel_module_frame/hello1.c
@@ -3,22 +3,8 @@
 #include <linux/init.h>      // included for __init and __exit macros
 #include "hello2.h"
 
-MODULE_LICENSE("GPL");
-MODULE_AUTHOR("clpsz");
-MODULE_DESCRIPTION("A Simple Hello World module");
-
-static int __init hello_init(void)
+void printk_in_hello1(void)
 {
-    printk(KERN_INFO "Hello world!\n");
-    printk_in_hello2();
-    return 0;    // Non-zero return means that the module couldn't be loaded.
+    printk(KERN_INFO "Calling printk in hello1.c\n");
 }
-
-static void __exit hello_cleanup(void)
-{
-    printk(KERN_INFO "Cleaning up module.\n");
-}
-
-module_init(hello_init);
-module_exit(hello_cleanup);
 
---
+++ kernel_module_frame/hello1.h
@@ -0,0 +1,8 @@
+#ifndef __HELLO1_H
+#define __HELLO1_H
+
+void printk_in_hello1(void);
+
+#endif
+
+
