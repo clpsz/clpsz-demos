7584a5bdf92e497701146237087fe3747b3c5724
zuopeter <zuopeter@fenqile.com>
2015-09-23 09:50:45 +0800

    Add example for multi processes

---
+++ C#/multi-process/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process proc = Process.Start("notepad.exe");
+        }
+    }
+}
