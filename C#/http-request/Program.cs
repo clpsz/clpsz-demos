003c77a2a59d84568b919e76a37199694112fade
clpsz <clpsz@gmail.com>
2015-09-23 10:57:40 +0800

    Add example for http request

---
+++ C#/http-request/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class MyTcpListener
+{
+    public static void Main()
+    {
+        string url = "http://www.qq.com/";
+        var request = (HttpWebRequest)WebRequest.Create(url);
+
+        var response = (HttpWebResponse)request.GetResponse();
+        Stream stream = response.GetResponseStream();
+        StreamReader sr = new StreamReader(stream);
+        string content = sr.ReadToEnd();
+
+        Console.Write(content);
+    }
+}
