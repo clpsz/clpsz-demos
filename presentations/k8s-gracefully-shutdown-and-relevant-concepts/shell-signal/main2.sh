125da5da9d1b290d22714fab0fae98a0de7d12f9
Peter Zuo <peter.zuo@shopee.com>
2021-12-22 15:55:03 +0800

    update main2.sh signal handler

--- presentations/k8s-gracefully-shutdown-and-relevant-concepts/shell-signal/main2.sh
+++ presentations/k8s-gracefully-shutdown-and-relevant-concepts/shell-signal/main2.sh
@@ -3,22 +3,26 @@
 
 echo "begin"
 
-#sh /app/scripts/start1.sh &
-#echo "start1.sh executed"
+stop=0
+
 
 handle_sigterm() {
   echo "[INFO] Received INT"
-  sleep 30
+  stop=1
 }
 trap handle_sigterm INT # 捕获 SIGINT 信号并回调 handle_sigterm 函数
 
 
 for a in `seq 10000`
 do
-   echo "hello world."
-   sleep 3
-   echo "hello shopee."
-   sleep 3
+  if [ "$stop" = "1" ]; then
+      break
+  fi
+
+  echo "hello world."
+  sleep 3
+  echo "hello shopee."
+  sleep 3
 done
 
 echo "end"
